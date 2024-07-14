bc2e10e2446c24c7c406711b48e60e0aa53d1b39
austin_m <1austinm@protonmail.com>
2024-07-14 14:48:40 -0500

    7.03

--- exercises/07.forms/03.problem.types/index.tsx
+++ exercises/07.forms/03.problem.types/index.tsx
@@ -13,7 +13,31 @@ function App() {
 				- photo (restricts the input to only accept image files)
 				- color
 				- startDate
-			 */}
+			 */}			 
+			 <div>
+				<label htmlFor='password'>Password:</label>
+				<input type="password" name='password' id='password'/>
+			 </div>
+
+			 <div>
+				<label htmlFor='age'>Age:</label>
+				<input type="number" min={0} max={200} name='age' id='age'/>
+			 </div>
+
+			 <div>
+				<label htmlFor=''>Photo</label>
+				<input type="file" name='profilePhoto' id='profilePhoto' accept='image/*'/>
+			 </div>
+
+			 <div>
+				<label htmlFor='favColor'>Favorite Color:</label>
+				<input type="color" name='favColor' id='favColor'/>
+			 </div>
+
+			 <div>
+				<label htmlFor='startDate'>Start Date:</label>
+				<input type="date" name='startDate' id='startData'/>
+			 </div>
 			<button type="submit">Submit</button>
 		</form>
 	)
